4355c155caecc5e780534a31738cec685bc4420e
minhaj321 <minhajsohail2020>
2020-11-14 19:04:30 +0500

    Updates

---
+++ precache-manifest.a704347bd945efa72f1113be2e765e88.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1455dec12d5f90d91ac768979e2e5a75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59d383b13b8bfe3ae2da",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "426a50668615bda4ff84",
+    "url": "/static/css/main.d6af5995.chunk.css"
+  },
+  {
+    "revision": "59d383b13b8bfe3ae2da",
+    "url": "/static/js/2.0d5d04e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0d5d04e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "426a50668615bda4ff84",
+    "url": "/static/js/main.d9d79c38.chunk.js"
+  },
+  {
+    "revision": "fe8fa407a2098f0849f8",
+    "url": "/static/js/runtime-main.75538023.js"
+  },
+  {
+    "revision": "df0fa5922be0460ee6eaa3fd9864b1cb",
+    "url": "/static/media/after_header.df0fa592.png"
+  },
+  {
+    "revision": "e454ddd2889c225680750c0ffdcdb00d",
+    "url": "/static/media/before_header.e454ddd2.png"
+  },
+  {
+    "revision": "a67e683b174c919a08145b7fd5050869",
+    "url": "/static/media/detail_ads_img.a67e683b.png"
+  },
+  {
+    "revision": "20e2cb8b09be0cd27b5e96d3d90323ec",
+    "url": "/static/media/display.20e2cb8b.png"
+  },
+  {
+    "revision": "d281caf459abaa8a900323651070e31d",
+    "url": "/static/media/sam-fold.d281caf4.jpg"
+  }
+]);
--- src/components/Before_Advertising.js
+++
@@ -1,7 +0,0 @@
-import React ,{Component} from 'react';
-import Advertise from './../images/after_header.png';
-export default class Advertising extends Component{
-	render(){
-		return(			<img src={Advertise} alt="Adds"/>			)
-	}
-}
